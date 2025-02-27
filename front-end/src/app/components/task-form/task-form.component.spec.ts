fde236784265db06fe47c9439d384822df28483f
SajidAhmed0 <mjsahmed205@gmail.com>
2025-02-28 03:39:52 +0530

    test(front-end): add test cases

--- front-end/src/app/components/task-form/task-form.component.spec.ts
+++ front-end/src/app/components/task-form/task-form.component.spec.ts
@@ -1,23 +1,98 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskFormComponent } from './task-form.component';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
   let fixture: ComponentFixture<TaskFormComponent>;
+  let taskService: jasmine.SpyObj<TaskService>;
 
   beforeEach(async () => {
+    const taskServiceSpy = jasmine.createSpyObj('TaskService', ['createTask']);
+
     await TestBed.configureTestingModule({
-      imports: [TaskFormComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        TaskFormComponent, 
+        FormsModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatButtonModule, 
+        HttpClientTestingModule,
+        NoopAnimationsModule
+      ],
+      providers: [{ provide: TaskService, useValue: taskServiceSpy }],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TaskFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    taskService = TestBed.inject(TaskService) as jasmine.SpyObj<TaskService>;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render form elements', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Create a Task');
+    expect(compiled.querySelector('input[matInput]')).toBeTruthy();
+    expect(compiled.querySelector('textarea[matInput]')).toBeTruthy();
+    expect(compiled.querySelector('button[mat-raised-button]')).toBeTruthy();
+  });
+
+  it('should disable submit button when title and description are empty', () => {
+    component.title.set('');
+    component.description.set('');
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTruthy();
+  });
+
+  it('should enable submit button when title and description are filled', () => {
+    component.title.set('New Task');
+    component.description.set('Task Description');
+    fixture.detectChanges();
+    
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeFalsy();
+  });
+
+  // it('should call `createTask` on form submit', () => {
+  //   component.title.set('New Task');
+  //   component.description.set('Task Description');
+  //   fixture.detectChanges();
+
+  //   const newTask: Task = { title: 'New Task', description: 'Task Description' };
+
+  //   taskService.createTask.and.returnValue(of(newTask));
+
+    
+  //   component.addTask();
+
+  //   expect(taskService.createTask).toHaveBeenCalledWith(newTask);
+  // });
+
+  // it('should emit `taskAdded` event when a task is successfully created', () => {
+  //   spyOn(component.taskAdded, 'emit');
+
+  //   component.title.set('New Task');
+  //   component.description.set('Task Description');
+  //   fixture.detectChanges();
+
+  //   taskService.createTask.and.returnValue(of({ title: 'New Task', description: 'Task Description' }));
+
+  //   component.addTask();
+
+  //   expect(component.taskAdded.emit).toHaveBeenCalled();
+  // });
 });
