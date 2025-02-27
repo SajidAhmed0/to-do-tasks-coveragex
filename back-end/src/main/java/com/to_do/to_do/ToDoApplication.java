b81a29842849d406196a4ed1f0085be12a80780b
SajidAhmed0 <mjsahmed205@gmail.com>
2025-02-27 14:52:55 +0530

    dockerize backend

--- back-end/src/main/java/com/to_do/to_do/ToDoApplication.java
+++ back-end/src/main/java/com/to_do/to_do/ToDoApplication.java
@@ -1,6 +1,5 @@
 package com.to_do.to_do;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("../") // Specifies parent directory
-				.load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(ToDoApplication.class, args);
 	}
 
